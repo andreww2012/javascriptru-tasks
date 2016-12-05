b561d0da454e9f8d6a542023022314993810d2c9
Andrew Kazakov <tosk-nn@yandex.ru>
2016-12-05 23:21:01 +0300

    All "string" tasks are done

---
+++ 3-data-structures/string-2.js
@@ -0,0 +1,7 @@
+// Напишите функцию checkSpam(str), которая возвращает true, если строка str содержит „viagra“ или „XXX“, а иначе false.
+
+function checkSpam(str) {
+  var strLow = str.toLowerCase();
+
+  return !!(~strLow.indexOf("viagra") || ~strLow.indexOf("xxx"));
+}
---
+++ 3-data-structures/string-3.js
@@ -0,0 +1,5 @@
+// Создайте функцию truncate(str, maxlength), которая проверяет длину строки str, и если она превосходит maxlength – заменяет конец str на "...", так чтобы ее длина стала равна maxlength. Результатом функции должна быть (при необходимости) усечённая строка.
+
+function truncate(str, maxLength) {
+  return (str.length > maxLength) ? (str.slice(0, maxLength - 3) + "...") : str;
+}

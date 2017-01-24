901b6e4fd04199471612c67700780711e67bedd5
Andrew Kazakov <tosk-nn@yandex.ru>
2017-01-24 15:03:10 +0300

    Done: all tasks from "object-methods" section

---
+++ 5-objects-more/object-methods-1.js
@@ -0,0 +1,23 @@
+// Создайте объект calculator с тремя методами:
+
+// read() запрашивает prompt два значения и сохраняет их как свойства объекта
+// sum() возвращает сумму этих двух значений
+// mul() возвращает произведение этих двух значений
+
+var calculator = {
+  val1: "",
+  val2: "",
+
+  read: function() {
+    this.val1 = +prompt("Первое число:", 2);
+    this.val2 = +prompt("Второе число:", 2);
+  },
+
+  sum: function() {
+    return this.val1 + this.val2;
+  },
+
+  mul: function() {
+    return this.val1 * this.val2;
+  }
+};

ec7e8cea3e9543d04e43d658571fb2732e74013f
Andrey Kazakov <tosk-nn@yandex.ru>
2017-01-24 19:47:12 +0300

    Done: all tasks from "constructor-new" section; small changes in run-any-script.html

---
+++ 5-objects-more/constructor-new-2.js
@@ -0,0 +1,24 @@
+// Напишите функцию-конструктор Calculator, которая создает объект с тремя методами:
+// Метод read() запрашивает два значения при помощи prompt и запоминает их в свойствах объекта.
+// Метод sum() возвращает сумму запомненных свойств.
+// Метод mul() возвращает произведение запомненных свойств.
+// Пример использования:
+/*var calculator = new Calculator();
+calculator.read();
+alert( "Сумма=" + calculator.sum() );
+alert( "Произведение=" + calculator.mul() );*/
+
+function Calculator() {
+  this.read = function() {
+    this.val1 = +prompt("Первое число:", 2);
+    this.val2 = +prompt("Второе число:", 2);
+  };
+
+  this.sum = function() {
+    return this.val1 + this.val2;
+  };
+
+  this.mul = function() {
+    return this.val1 * this.val2;
+  };
+}

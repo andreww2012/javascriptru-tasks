bc00aabb70fc77edddd5b47d253f22fea061b239
Andrew Kazakov <tosk-nn@yandex.ru>
2017-01-31 23:05:34 +0300

    Done: task from "descriptors-getters-setters" section

---
+++ 5-objects-more/descriptors-getters-setters.js
@@ -0,0 +1,45 @@
+// Вам попал в руки код объекта User, который хранит имя и фамилию в свойстве this.fullName:
+/*
+function User(fullName) {
+  this.fullName = fullName;
+}
+
+var vasya = new User("Василий Попкин");
+*/
+// Имя и фамилия всегда разделяются пробелом.
+// Сделайте, чтобы были доступны свойства firstName и lastName, причём не только на чтение, но и на запись, вот так:
+/*
+var vasya = new User("Василий Попкин");
+
+// чтение firstName/lastName
+alert( vasya.firstName ); // Василий
+alert( vasya.lastName ); // Попкин
+
+// запись в lastName
+vasya.lastName = 'Сидоров';
+
+alert( vasya.fullName ); // Василий Сидоров
+*/
+// Важно: в этой задаче fullName должно остаться свойством, а firstName/lastName – реализованы через get/set. Лишнее дублирование здесь ни к чему.
+
+function User(fullName) {
+  this.fullName = fullName;
+}
+
+Object.defineProperty(User, firstName, {
+  get: function() {
+    return this.fullName.split(" ")[0];
+  },
+  set: function(str) {
+    this.fullName = str + " " + this.lastName;
+  }
+});
+
+Object.defineProperty(user, lastName, {
+  get: function() {
+    return this.fullName.split(" ")[1];
+  },
+  set: function(str) {
+    this.fullName = this.firstName + " " + str;
+  }
+});

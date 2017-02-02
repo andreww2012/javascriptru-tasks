a5977cc45d8dc3bca497fc0186cad7dc62cb59e6
Andrew Kazakov <tosk-nn@yandex.ru>
2017-02-03 00:28:44 +0300

    Done: all tasks from "decorators" section

---
+++ 5-objects-more/decorators-2.js
@@ -0,0 +1,10 @@
+// Создайте декоратор makeLogging(func, log), для функции func возвращающий обёртку,
+// которая при каждом вызове добавляет её аргументы в массив log.
+// Условие аналогично задаче Логирующий декоратор (1 аргумент), но допускается func с любым набором аргументов.
+
+function makeLogging(f, log) {
+  return function() {
+    log.push([].slice.call(arguments));
+    return f.apply(this, arguments);
+  };
+}

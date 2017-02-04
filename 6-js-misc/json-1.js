989aaffe12dff125a84765aebdcd52c76cc19051
Andrew Kazakov <tosk-nn@yandex.ru>
2017-02-05 00:43:44 +0300

    Done: all tasks from "json" section

---
+++ 6-js-misc/json-1.js
@@ -0,0 +1,17 @@
+// Превратите объект leader из примера ниже в JSON:
+/*
+var leader = {
+  name: "Василий Иванович",
+  age: 35
+};
+*/
+// После этого прочитайте получившуюся строку обратно в объект.
+
+var leader = {
+  name: "Василий Иванович",
+  age: 35
+};
+
+leaderJSON = JSON.stringify(leader);
+
+leader = JSON.parse(leaderJSON);
---
+++ 6-js-misc/json-2.js
@@ -0,0 +1,20 @@
+// Превратите объект team из примера ниже в JSON:
+
+var leader = {
+  name: "Василий Иванович"
+};
+
+var soldier = {
+  name: "Петька"
+};
+
+// эти объекты ссылаются друг на друга!
+leader.soldier = soldier;
+soldier.leader = leader;
+
+var team = [leader, soldier];
+
+// НАЧАЛО РЕШЕНИЯ
+// Сделать прямой вызов JSON.stringify(team) нельзя, поскольку объекты ссылаются друг на друга, получится
+// бесконечная JSON-строка
+teamJSON = JSON.stringify(team, ["name"]);

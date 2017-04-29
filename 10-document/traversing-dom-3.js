31cb350a7a0b9cd14cccc1f4b5e57838faa2d6e3
Andrew Kazakov <tosk-nn@yandex.ru>
2017-04-29 12:23:05 +0300

    Fixed traversing-dom-3.js

--- 10-document/traversing-dom-3.js
+++ 10-document/traversing-dom-3.js
@@ -2,6 +2,6 @@
 // Вам нужно будет получить из таблицы table все диагональные td и выделить их, используя код:
 // td.style.backgroundColor = 'red';
 
-for (int i = 0; i < table.rows.length; i++) {
+for (var i = 0; i < table.rows.length; i++) {
   table.rows[i].cells[i].style.backgroundColor = "red";
 }

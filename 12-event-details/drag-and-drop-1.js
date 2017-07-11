7044f02b66c3e505ea1da775193e71d3355e7948
Andrey Kazakov <tosk-nn@yandex.ru>
2017-07-11 17:57:59 +0300

    Done 1st task from "drag-and-drop" section

---
+++ 12-event-details/drag-and-drop-1.js
@@ -0,0 +1,33 @@
+// Создайте слайдер: https://vk.cc/6RELff
+// Важно:
+// * Слайдер должен нормально работать при резком движении мыши влево или
+// вправо, за пределы полосы. При этом бегунок должен останавливаться четко в
+// нужном конце полосы.
+// * При нажатом бегунке мышь может выходить за пределы полосы слайдера, но
+// слайдер пусть все равно работает (это удобно для пользователя).
+
+let slider = document.getElementById("slider");
+let sliderThumb = slider.firstElementChild;
+
+sliderThumb.ondragstart = () => false;
+
+sliderThumb.onmousedown = (e) => {
+
+  let sliderCoords = slider.getBoundingClientRect();
+  let sliderThumbCoords = sliderThumb.getBoundingClientRect();
+  let shiftX = e.pageX - sliderThumbCoords.left;
+
+  document.onmousemove = (e) => {
+    sliderThumb.style.left = Math.min(
+      Math.max(0, e.pageX - sliderCoords.left),
+      slider.offsetWidth - sliderThumb.offsetWidth
+    ) + "px";
+  };
+
+  document.onmouseup = (e) => {
+    document.onmousemove = null;
+    document.onmouseup = null;
+  };
+
+  return false;
+};

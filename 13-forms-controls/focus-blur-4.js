587daacbd1a0803da43a38b9914437ecf9fa434f
Andrey Kazakov <tosk-nn@yandex.ru>
2017-07-14 21:35:31 +0300

    Done: all tasks from "focus-blur" section

--- 13-forms-controls/focus-blur-4.js
+++ 13-forms-controls/focus-blur-4.js
@@ -5,4 +5,47 @@
 // * При редактировании под ячейкой появляются кнопки для приема и отмена
 // редактирования, только клик на них заканчивает редактирование.
 
-// TODO
+let table = document.getElementById("bagua-table");
+let editingTd = null;
+
+table.onclick = e => {
+  let target = e.target;
+
+  if (target.classList.contains("edit-ok")) {
+    editingTd.elem.innerHTML = editingTd.elem.firstChild.value;
+    editingTd.elem.classList.remove("edit-td");
+    editingTd = null;
+    return;
+  }
+
+  if (target.classList.contains("edit-cancel")) {
+    editingTd.elem.innerHTML = editingTd.data;
+    editingTd.elem.classList.remove("edit-td");
+    editingTd = null;
+    return;
+  }
+
+  if (target.nodeName != "TD" || editingTd) {
+    return;
+  }
+
+  editingTd = {
+    elem: target,
+    data: target.innerHTML
+  }
+  target.classList.add("edit-td");
+
+  let textarea = document.createElement("textarea");
+  textarea.style.width = target.offsetWidth + "px";
+  textarea.style.height = target.offsetHeight + "px";
+  textarea.className = "edit-area";
+  textarea.value = target.innerHTML;
+  target.innerHTML = "";
+  target.appendChild(textarea);
+  textarea.focus();
+
+  target.insertAdjacentHTML(
+    "beforeEnd",
+    '<div class="edit-controls"><button class="edit-ok">OK</button><button class="edit-cancel">CANCEL</button></div>'
+  );
+};
--- 13-forms-controls/focus-blur-6.js
+++ 13-forms-controls/focus-blur-6.js
@@ -2,4 +2,56 @@
 // CapsLock. Выключение CapsLock уберёт предупреждение.
 // Такое поле может помочь избежать ошибок при вводе пароля.
 
-// TODO
+let input = document.getElementsByTagName("input")[0];
+let capsMessage = document.getElementById("caps");
+
+// null = неизвестно, true/false = нажат/не нажат
+let capsPressed = null;
+
+function changeCapsMessage(state) {
+  let display = (state == undefined) ? capsPressed : state;
+  capsMessage.style.display = display ? "block" : "none";
+}
+
+// Функция из статьи на сайте
+function getChar(event) {
+  if (event.which == null) { // IE
+    if (event.keyCode < 32) return null; // спец. символ
+    return String.fromCharCode(event.keyCode)
+  }
+
+  if (event.which != 0 && event.charCode != 0) { // все кроме IE
+    if (event.which < 32) return null; // спец. символ
+    return String.fromCharCode(event.which); // остальные
+  }
+
+  return null; // спец. символ
+}
+
+document.addEventListener("keydown", (e) => {
+  if (e.keyCode == 20 && capsPressed != null) {
+    capsPressed = !capsPressed;
+  }
+});
+
+document.addEventListener("keypress", (e) => {
+  let char = getChar(e);
+
+  // Отбрасываем служебные символы и символы без регистров
+  if (!(char == null || char.toLowerCase() == char.toUpperCase())) {
+    capsPressed = ((!e.shiftKey && char == char.toUpperCase())
+      || (e.shiftKey && char == char.toLowerCase()));
+  }
+});
+
+input.addEventListener("keyup", (e) => {
+  changeCapsMessage();
+});
+
+input.addEventListener("focus", (e) => {
+  changeCapsMessage();
+}, true); // не всплывает!
+
+input.addEventListener("blur", (e) => {
+  changeCapsMessage(false);
+}, true); // не всплывает!
